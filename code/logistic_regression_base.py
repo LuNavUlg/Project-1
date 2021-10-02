f806d532496341fc66c7f2b0af4b6a5a6056866b
LuNavUlg <47796404+LuNavUlg@users.noreply.github.com>
2021-10-02 13:12:51 +0200

    logistic regression

--- code/logistic_regression_base.py
+++ code/logistic_regression_base.py
@@ -8,7 +8,6 @@ Project 1 - Classification algorithms
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 from plot import plot_boundary
 from data import make_balanced_dataset, make_unbalanced_dataset
@@ -16,44 +15,6 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
 
-def conditional_propabilty_of_positive_class(x, theta):
-    """Computes conditional probability of sample x belonging to the positive
-        class knowing parameter theta and data sample X[i, :]
-
-    Parameters
-    ----------
-    x : vector-like, shape = [n_features]
-        The sample.
-
-    theta : vector-like, [omega_0, omega^T (vetor)]
-        Parameters of the sigmoïd.
-
-    Returns
-    -------
-    p : conditional probability of sample x belonging to the positive
-        class knowing parameter theta and data sample X[i, :]
-    """
-
-    w_0 = theta[0] # real value
-    w = theta[1] # vector
-
-    p = 1/(1+exp(-w_0 - np.dot(np.transpose(w), x)))
-    return p
-
-def gradient_of_loss_function(X, theta):
-    sum = 0
-    N = np.shape(X)
-    for i in range(N):
-        x_prime = np.transpose(np.append(1, X[i]))
-        sum = sum + (conditional_propabilty_of_positive_class(X[i], )-y[i])*x_prime[i]
-    return (1/N)*sum
-
-def loss_function(theta, X):
-    sum = 0
-    N = np.shape(X)
-    for i in range(N):
-        sum = sum + log((conditional_propabilty_of_positive_class(X[i], theta))
-    return -(1/N)*sum
 
 class LogisticRegressionClassifier(BaseEstimator, ClassifierMixin):
 
@@ -96,24 +57,6 @@ class LogisticRegressionClassifier(BaseEstimator, ClassifierMixin):
 
         # TODO insert your code here
 
-        # Gradient descent to compute possible values of theta
-        theta = []
-        for i in range(self.n_iter):
-            theta_new = theta_old - self.learning_rate*gradient_loss(theta_old)
-            np.append(theta, theta_new)
-            theta_old = theta_new
-
-        # Now compute loss function for all values of theta
-        loss_functions = []
-        for theta_val in theta:
-            np.append(loss_functions, loss_function(theta_val, X))
-
-        # Find minimum loss function
-        min_index = argmin(loss_functions)
-        # Find corresponding theta value
-        optimal_theta = theta[min_index]
-
-        self.BaseEstimator.set_params(optimal_theta)
         return self
 
 
@@ -132,16 +75,7 @@ class LogisticRegressionClassifier(BaseEstimator, ClassifierMixin):
         """
 
         # TODO insert your code here
-        y = []
-        size = np.shape(X)
-        theta = self.BaseEstimator.get_params()
-        for i in range(size[0]):
-            if conditional_propabilty_of_positive_class(X[i], theta) >= 0.5:
-                y[i] = +1
-            else:
-                y[i] = -1
-
-        return y
+        pass
 
     def predict_proba(self, X):
         """Return probability estimates for the test data X.
